f1057f02e931eb2d9e612b7f602e986a4fc0c82e
kuznetsov-radio <51828521+kuznetsov-radio@users.noreply.github.com>
2020-02-04 11:44:03 +0800

    Add files via upload

--- getparms.cpp
+++ getparms.cpp
@@ -25,7 +25,7 @@ const char* arr[]={
  "    theta_b;    90.0       ;degrees;                 Angle of beam direction",
  "        dMu;   0.100          ;none;         dMu for gau/exp/SuGau loss-cone",
  "        a_4;    10.0          ;none;       Coeff for a*(Mu-xMu0)^4 for SuGau",
- "   Nthreads;           0      ;none;    Number of parallel threads (0: auto)",
+ "   reserved;           0      ;none;                                reserved",
  "     f^C_cr;           0      ;f_ce;          Hybrid code boundary frequency",
  "    f^WH_cr;           0      ;f_ce;            Exact J_s boundary frequency",
  " Match. key;           1       ;1/0;                  Renormalization on/off",
@@ -33,11 +33,14 @@ const char* arr[]={
  "        n_H;           0   ;cm^{-3};          Neutral hydrogen concentration",
  "        n_p;           0   ;cm^{-3};                   Protons concentration",
  "  Array key;           0       ;1/0;                            Array on/off",
- "     mu key;           2     ;2/1/0;        Exact (2) / force isotropic (<2)",
- "    log key;           0       ;1/0;       Equidistant / logarithmic spacing"
+ "     mu key;           2     ;2/1/0;      Exact (2) / Isotropic (1) / PK (0)",
+ "    log key;           0       ;1/0;       Equidistant / logarithmic spacing",
+ "   freq key;           0       ;1/0;         User-defined / log-spaced array",
+ "   reserved;           0      ;none;                                reserved",
+ " interp key;           0       ;1/0;               LQ / spline interpolation"
 };
 
-#define Nstrings 34
+#define Nstrings 37
 
 #ifndef LINUX
 extern "C" __declspec(dllexport) float GET_PARMS(int argc, void **argv)
